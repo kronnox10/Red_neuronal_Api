bebb2081d40228ec1254d1d03385171aeb213bcd
kronnox10 <134155626+kronnox10@users.noreply.github.com>
2025-07-19 17:36:19 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,6 +33,7 @@ app.add_middleware(
    allow_origins=[
          "http://localhost:5173",
         "http://localhost:3000",
+        "http://localhost:4200",
         "capacitor://localhost",      #
         "https://localhost",         
         "https://red-neuronal-api.onrender.com",
